7470e1cd49e562bee7fc84bef6318d4b47ff5c6b
Diego Castillo <diego.castillo@tektonlabs.com>
2021-06-17 17:58:16 -0500

    Adding migrations and base EF

--- src/CleanArchitecture.Store.Domain/Entities/Category.cs
+++ src/CleanArchitecture.Store.Domain/Entities/Category.cs
@@ -7,7 +7,7 @@ namespace CleanArchitecture.Store.Domain.Entities
     public class Category : AuditableEntity
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; }
         public string Provider { get; set; }
         public DateTime EndOfContract { get; set; }
         public ICollection<Product> Products { get; set; }
